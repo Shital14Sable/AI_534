2323d69c6e25d1a86eae98a3ce655458d1656323
Shital Dnyandeo Sable <57466284+Shital14Sable@users.noreply.github.com>
2022-12-02 20:18:43 -0800

    Update bag_of_words.py

--- IA4/bag_of_words.py
+++ IA4/bag_of_words.py
@@ -18,6 +18,8 @@ from sklearn.linear_model import LogisticRegression
 import gensim.downloader as api
 import string
 from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.naive_bayes import MultinomialNB
+from sklearn.ensemble import RandomForestClassifier
 
 
 ge = api.load("glove-twitter-200")
@@ -45,13 +47,21 @@ def create_BoW(input_data, test_data):
         tweet_i = words_of_tweet.join(tweet_i)
         input_data.loc[i, 'text'] = tweet_i
 
+    for j in range(test_data.shape[0]):
+        tweet_j = test_data.text[j]
+        tweet_j = text_cleanup(tweet_j)
+        words_of_tweet_j = " "
+        tweet_j = words_of_tweet_j.join(tweet_j)
+        test_data.loc[j, 'text'] = tweet_j
+
+
     # CountVectorizer
     vect_elem_cv = CountVectorizer()  # create an object
     train_words_X = vect_elem_cv.fit_transform(input_data['text'])
     train_words_Y = input_data['sentiment']
+    bag_of_words = pd.DataFrame(train_words_X.toarray(), columns=vect_elem_cv.get_feature_names())
     test_X = vect_elem_cv.transform(test_data['text'])
     test_Y = test_data['sentiment']
-    bag_of_words = vect_elem_cv.vocabulary_
 
     return train_words_X, train_words_Y, test_X, test_Y, bag_of_words
 
@@ -60,15 +70,30 @@ def rep_new_tweets(train_words_X, train_words_Y, test_X, test_Y):
     reg_model = LogisticRegression()
     reg_model.fit(train_words_X, train_words_Y)
     train_score = reg_model.score(train_words_X, train_words_Y)
-    reg_model.fit(test_X, test_Y)
     test_score = reg_model.score(test_X, test_Y)
     return train_score, test_score
+
+
+def nb_classifier(train_words_X, train_words_Y, test_X, test_Y):
+    nb_ist = MultinomialNB()
+    nb_ist.fit(train_words_X, train_words_Y)
+    train_scor_nb = nb_ist.score(train_words_X, train_words_Y)
+    test_score_nb = nb_ist.score(test_X, test_Y)
+    return train_scor_nb, test_score_nb
+
+
+def rf_classifier(train_words_X, train_words_Y, test_X, test_Y):
+    rf_ist = RandomForestClassifier()
+    rf_ist.fit(train_words_X, train_words_Y)
+    train_scor_rf = rf_ist.score(train_words_X, train_words_Y)
+    test_score_rf = rf_ist.score(test_X, test_Y)
+    return train_scor_rf, test_score_rf
 
 
 if __name__ == "__main__":
     train_data = pd.read_csv("./IA3-train.csv")
     test_data = pd.read_csv("./IA3-dev.csv")
-    train_words_X, train_words_Y, test_X, test_Y, bag_of_words = create_BoW(train_data,test_data)
-    print(rep_new_tweets(train_words_X, train_words_Y, test_X, test_Y,))
-
-
+    train_words_X, train_words_Y, test_X, test_Y, bag_of_words = create_BoW(train_data, test_data)
+    print(rep_new_tweets(train_words_X, train_words_Y, test_X, test_Y))
+    print(nb_classifier(train_words_X, train_words_Y, test_X, test_Y))
+    print(rf_classifier(train_words_X, train_words_Y, test_X, test_Y))
